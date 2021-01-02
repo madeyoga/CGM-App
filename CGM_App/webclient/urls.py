b20e27e1679bd8769aada4fc87506a5468f12bd8
MadeYoga <vngla21@gmail.com>
2021-01-02 20:19:04 +0700

    Add route for edit and delete item

--- CGM_App/webclient/urls.py
+++ CGM_App/webclient/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('pembeli/', views.index, name='webclient_pembeli'),
     path('penjualan/', views.index, name='webclient_penjualan'),
     path('post_item/', views.post_item, name='webclient_post_item'),
+    path('edit_item/', views.edit_item, name='webclient_edit_item'),
+    path('delete_item/<int:item_id>', views.delete_item, name='webclient_delete_item'),
 ]
