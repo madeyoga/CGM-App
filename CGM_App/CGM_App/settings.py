9cf1cf8b184874b59418f1b714b64d2fd9d6b4e8
MadeYoga <vngla21@gmail.com>
2021-01-18 16:36:45 +0700

    Add static root and media url & root to settings

--- CGM_App/CGM_App/settings.py
+++ CGM_App/CGM_App/settings.py
@@ -129,4 +129,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
 SECURE_SSL_REDIRECT = False
